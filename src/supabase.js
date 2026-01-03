71a8eb87dd2b93f3db60ac259c80639fb12ef8d7
Sejan Randinu <sejanrandinu01@gmail.com>
2026-01-03 16:09:46 +0530

    init7

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://jtdqwusyhqbjlefyboyt.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0ZHF3dXN5aHFiamxlZnlib3l0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcyMDI5NDEsImV4cCI6MjA4Mjc3ODk0MX0.HyLZsq_5zEUksrI1qJXfZYtAI0EFdrPJaAp1yZyQkBc'
+const supabaseUrl = process.env.VITE_SUPABASE_URL
+const supabaseKey = process.env.VITE_SUPABASE_KEY
 
 if (!supabaseUrl || !supabaseKey) {
     console.error('Supabase credentials missing! Check environment variables.')
