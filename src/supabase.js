d9b5f0b8ff8b95e3d97cb3add5b1f958ca4de9e2
Sejan Randinu <sejanrandinu01@gmail.com>
2026-01-03 15:52:06 +0530

    init new 1

--- src/supabase.js
+++ src/supabase.js
@@ -1,14 +1,11 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const supabaseUrl = 'https://jtdqwusyhqbjlefyboyt.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0ZHF3dXN5aHFiamxlZnlib3l0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcyMDI5NDEsImV4cCI6MjA4Mjc3ODk0MX0.HyLZsq_5zEUksrI1qJXfZYtAI0EFdrPJaAp1yZyQkBc'
 
 if (!supabaseUrl || !supabaseKey) {
     console.error('Supabase credentials missing! Check environment variables.')
 }
 
-export const supabase = createClient(
-    supabaseUrl || 'https://placeholder.supabase.co',
-    supabaseKey || 'placeholder-key'
-)
+export const supabase = createClient(supabaseUrl, supabaseKey)
